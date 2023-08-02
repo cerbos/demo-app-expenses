60ab2a209d796ede58962280a134626082a8fb13
Alex Olivier <alex@alexolivier.me>
2023-08-02 12:26:24 +0100

    updates
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- server/src/tracing.ts
+++ server/src/tracing.ts
@@ -1,25 +1,39 @@
-import 'dotenv/config'
+import "dotenv/config";
 
-import { instrumentations } from './instrumentations' // must be imported before OpenTelemetry
+import { instrumentations } from "./instrumentations"; // must be imported before OpenTelemetry
 
-import { diag, DiagConsoleLogger, DiagLogLevel, trace } from '@opentelemetry/api'
-import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks'
-import { NodeSDK } from '@opentelemetry/sdk-node'
+import {
+  diag,
+  DiagConsoleLogger,
+  DiagLogLevel,
+  trace,
+} from "@opentelemetry/api";
+import { AsyncLocalStorageContextManager } from "@opentelemetry/context-async-hooks";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-grpc";
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const exporter = new OTLPTraceExporter({
+  url: process.env.OTLP_ENDPOINT,
+});
 
 const sdk = new NodeSDK({
   contextManager: new AsyncLocalStorageContextManager(),
   instrumentations,
-  serviceName: 'expenses-api',
-})
+  serviceName: "expenses-api",
+  traceExporter: exporter,
+});
 
 sdk.start();
 
-['SIGINT', 'SIGTERM'].forEach(signal => {
+["SIGINT", "SIGTERM"].forEach((signal) => {
   process.on(signal, () => {
-    sdk.shutdown().catch(console.error).finally(() => process.exit(0))
-  })
+    sdk
+      .shutdown()
+      .catch(console.error)
+      .finally(() => process.exit(0));
+  });
 });
 
-export const tracer = trace.getTracer('expenses-api')
+export const tracer = trace.getTracer("expenses-api");
